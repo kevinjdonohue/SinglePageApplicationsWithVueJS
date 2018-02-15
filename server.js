332999fa644a98349fdb142d1768b69d723e855b
Kevin Donohue <Kevin.Donohue@alaskaair.com>
2018-02-15 09:23:17 -0800

    updating the server to serve static content from the dist folder

--- server.js
+++ server.js
@@ -7,6 +7,9 @@ const indexHTML = (() => {
   return fs.readFileSync(path.resolve(__dirname, "./index.html"), "utf-8");
 })();
 
+// serves the static module of express to serve the statics from the dist folder
+app.use("/dist", express.static(path.resolve(__dirname, "./dist")));
+
 app.get("*", (request, response) => {
   response.write(indexHTML);
   response.end();
