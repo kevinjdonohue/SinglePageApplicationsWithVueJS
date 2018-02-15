d97a6076ff7f6511a1cc926da610994e02f5bd14
Kevin Donohue <Kevin.Donohue@alaskaair.com>
2018-02-15 09:24:31 -0800

    adding a TEMP solution to allow webpack to handle Vue (components correctly)
    real fix is to use .vue files for components?

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,8 +1,14 @@
 const path = require("path");
 
+// TODO:  resolve block below is a temporary solution; should use .vue files (components) instead
 const config = {
   entry: {
     app: path.resolve(__dirname, "../src/client-entry.js")
+  },
+  resolve: {
+    alias: {
+      vue: "vue/dist/vue.js"
+    }
   },
   output: {
     path: path.resolve(__dirname, "../dist"),
