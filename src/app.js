316d47971d4c0faed61b57762123e3824999aafe
Kevin Donohue <Kevin.Donohue@alaskaair.com>
2018-02-21 09:14:59 -0800

    disabling module transformation via babel
    turning on babel debugging output
    
    removing unnecessary eslint disable statement

--- src/app.js
+++ src/app.js
@@ -1,4 +1,4 @@
-/* eslint-disable no-console, import/prefer-default-export */
+/* eslint-disable import/prefer-default-export */
 import Vue from 'vue';
 import AppLayout from './theme/Layout.vue';
 
