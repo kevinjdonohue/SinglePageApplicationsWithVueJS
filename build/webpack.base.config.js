e575664ad857617986a13bd84b0a17296c23f10d
Kevin Donohue <Kevin.Donohue@alaskaair.com>
2018-02-23 10:28:30 -0800

    adding an additional webpack plugin so that we can emit our styles in a single, separate CSS file -- instead of having them bundled
    formatting double quotes to single quotes in webpack configs
    
    turned off babel debugging info -- shortens output in shell

--- build/webpack.base.config.js
+++ build/webpack.base.config.js
@@ -1,36 +1,36 @@
-const path = require("path");
+const path = require('path');
 
 const config = {
   entry: {
-    app: path.resolve(__dirname, "../src/client-entry.js")
+    app: path.resolve(__dirname, '../src/client-entry.js')
   },
   module: {
     rules: [
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /(\.js$)|(\.vue$)/,
-        loader: "eslint-loader",
+        loader: 'eslint-loader',
         exclude: /node_modules/
       },
       {
         test: /\.vue$/,
-        loader: "vue-loader",
+        loader: 'vue-loader',
         options: {
-          css: "css-loader",
-          scss: "css-loader|sass-loader"
+          css: 'css-loader',
+          scss: 'css-loader|sass-loader'
         }
       },
       {
         test: /\.js$/,
-        loader: "babel-loader",
+        loader: 'babel-loader',
         exclude: /node_modules/
       }
     ]
   },
   output: {
-    path: path.resolve(__dirname, "../dist"),
-    publicPath: "/",
-    filename: "assets/js/[name].js"
+    path: path.resolve(__dirname, '../dist'),
+    publicPath: '/',
+    filename: 'assets/js/[name].js'
   }
 };
 
--- build/webpack.client.config.js
+++ build/webpack.client.config.js
@@ -1,7 +1,16 @@
-const base = require("./webpack.base.config");
+const base = require('./webpack.base.config');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 const config = Object.assign({}, base, {
   plugins: base.plugins || []
 });
+
+config.module.rules
+  .filter(rule => {
+    return rule.loader === 'vue-loader';
+  })
+  .forEach(rule => (rule.options.extractCSS = true));
+
+config.plugins.push(new ExtractTextPlugin('assets/styles.css'));
 
 module.exports = config;
